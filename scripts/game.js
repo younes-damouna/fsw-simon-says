7436be08cb3b47a9ebc4d5c4fc2b4dfec515b5c2
younes_damouna <younes.damouna@gmail.com>
2023-11-13 20:58:37 +0200

    adding btn fo hardness that increase number of btns by 2

--- scripts/game.js
+++ scripts/game.js
@@ -11,11 +11,14 @@ let btns = document.getElementsByClassName('btn')
 let container = document.getElementsByClassName("container")[0]
 let sounds = [greenSound, redSound, yellowSound, blueSound]
 let sequence = []
+let frequencies = [0, 0, 0, 0]
 let userSequence = []
 let level = 0
 let levelTitle = document.getElementById("level-title")
 let gameOverBg = document.getElementsByTagName("body")[0]
 
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audio = new AudioContext();
 
 
 //  Adding click events to btns
@@ -30,7 +33,7 @@ for (let i = 0; i < btns.length; i++) {
             // removing the animation after 1 second
             setTimeout(() => {
                 btns[i].classList.remove("pressed")
-            }, 1000);
+            }, 500);
             insertStep(i)
 
         }
@@ -39,7 +42,7 @@ for (let i = 0; i < btns.length; i++) {
             btns[i].classList.add("pressed")
             setTimeout(() => {
                 btns[i].classList.remove("pressed")
-            }, 1000);
+            }, 500);
             insertStep(i)
         }
         else if (btns[i].getAttribute("id") == "red") {
@@ -47,7 +50,7 @@ for (let i = 0; i < btns.length; i++) {
             btns[i].classList.add("pressed")
             setTimeout(() => {
                 btns[i].classList.remove("pressed")
-            }, 1000);
+            }, 500);
 
             insertStep(i)
         }
@@ -56,10 +59,12 @@ for (let i = 0; i < btns.length; i++) {
             btns[i].classList.add("pressed")
             setTimeout(() => {
                 btns[i].classList.remove("pressed")
-            }, 1000);
-
+            }, 500);
             insertStep(i)
+
         }
+
+
     })
 }
 
@@ -67,9 +72,10 @@ for (let i = 0; i < btns.length; i++) {
 
 
 
+
 // this function will generate a number betwwen 0 and 4 and return the number
 function generateStep() {
-    let randomIndex = Math.floor(Math.random() * 4)
+    let randomIndex = Math.floor(Math.random() * btns.length)
     return randomIndex
 }
 
@@ -81,10 +87,13 @@ function insertStep(step) {
 
     //check if the added step is equal to the one in the sequence
     if (userSequence[index] !== sequence[index]) {
-        EndGame('Oops! Game over, you pressed the wrong tile');
+        endGame();
         wrongSound.play()
         return;
     }
+    console.log("userseq", userSequence[index], "seq", sequence[index])
+    console.log("userseq", userSequence, "seq", sequence)
+
 
     if (userSequence.length === sequence.length) {
         //if they are the same it will empty userSequence
@@ -122,19 +131,27 @@ function addEffects(sequence) {
 
 
 // when game ends
-function EndGame(text) {
-    //reset values
+function resetVariables() {
     sequence = [];
     userSequence = [];
     level = 0;
+    //adding the event listener
+
+    document.addEventListener("keydown", startGame)
+    levelTitle.textContent = 'Level hardness increased, Press any key to star';
+
+
+}
+function endGame() {
+    //reset values
+    resetVariables()
     levelTitle.textContent = 'Game Over, Press Any Key to Start';
     gameOverBg.classList.add("game-over")
-    //adding the event listener
-    document.addEventListener("keydown", startGame)
 }
 
 //this will start the game
 function startGame() {
+
     //removing the event listener
     document.removeEventListener("keydown", startGame)
     gameOverBg.classList.remove("game-over")
@@ -154,7 +171,7 @@ function startGame() {
 
 function autoClickBtn(btnIndex) {
     // playing sound and adding animation
-    setTimeout(() => (playBtnSound(sounds[btnIndex])), 0)
+    setTimeout(() => (playBtnSound(btnIndex)), 0)
     setTimeout(() => addClickAnimation(btns[btnIndex]), 0)
 
 
@@ -175,11 +192,101 @@ function removeClickAnimation(element) {
 }
 // play sound function
 function playBtnSound(sound) {
-    sound.play()
+    if (sound < 4) {
+        sounds[sound].play()
+
+    } else {
+        sounds[sound].play(frequencies[sound], 0.8, "square").stop(0.5);
+
+    }
+
+}
+
+//create a button that allow the user to increase dificulty by increasing the number of btns
+
+function increaseDifficulty() {
+
+    let container = document.getElementsByClassName("container")[0]
+    let row = document.createElement("div")
+    row.setAttribute("class", "row")
+
+    // Create 2 btns
+    for (let i = 0; i < 2; i++) {
+        let btn = document.createElement("div")
+        btn.setAttribute("type", "button")
+        btn.setAttribute("class", "btn")
+        // create a random rgb color
+        let redColor = Math.floor(Math.random() * 255)
+        let blueColor = Math.floor(Math.random() * 255)
+
+        let greenColor = Math.floor(Math.random() * 255)
+
+
+        let color = `rgb(${Math.abs(redColor)},${blueColor},${greenColor})`
+        btn.style.backgroundColor = color
+        row.appendChild(btn)
+        // create a sound for each btn
+
+
+        let randomFrequence = Math.floor(Math.random() * 1000)
+        frequencies.push(randomFrequence)
+
+        let note = (new SoundPlayer(audio))
+        sounds.push(note)
+
+    }
+
+    container.append(row)
+    for (let i = 0; i < btns.length; i++) {
+        if (i > 3) {
+            btns[i].addEventListener("click", function () {
+
+                sounds[i].play(frequencies[i], 0.8, "square").stop(0.5);
+
+                btns[i].classList.add("pressed")
+                setTimeout(() => {
+                    btns[i].classList.remove("pressed")
+                }, 1000);
+                insertStep(i)
+
+            })
+        }
+    }
+
+
+
+
+
 
 }
 
 
+
+
+//create a button to increase hardness
+let btnHard = document.createElement("button")
+btnHard.innerText = "Make it hard"
+btnHard.classList.add("btnHard")
+document.body.append(btnHard)
+
+// hard btn onclick
+btnHard.addEventListener("click", function () {
+    resetVariables()
+    increaseDifficulty()
+
+})
+
+//make a button to reset hardness
+let resetHardness = document.createElement("button")
+resetHardness.innerText = "Reset Hardness"
+resetHardness.classList.add("btnHard")
+document.body.append(resetHardness)
+
+// reset btn onclick
+resetHardness.addEventListener("click", function () {
+    window.location = "/"
+
+})
 
 document.addEventListener("keydown", startGame)
 
